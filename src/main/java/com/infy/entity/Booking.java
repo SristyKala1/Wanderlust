cb9d8f3510c0f59d37f5acf8b5c6a647368b9907
Sristy <kalasristy@gmail.com>
2022-09-17 01:33:04 +0530

    Coded entity file for booking feature

---
+++ src/main/java/com/infy/entity/Booking.java
@@ -0,0 +1,69 @@
+package com.infy.entity;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+
+@Entity
+@Table(name="BOOKING")
+public class Booking{
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Integer bookingId;
+	 private LocalDate checkIn;
+	 private LocalDate checkOut;
+	 private Integer noOfPeople;
+	 private Float totalCost;
+	 private LocalDateTime timeOfBooking = LocalDateTime.now();
+	 private Destination destinationEntity;
+	 private User userEntity;
+	 
+	public Integer getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+	public LocalDate getCheckIn() {
+		return checkIn;
+	}
+	public void setCheckIn(LocalDate checkIn) {
+		this.checkIn = checkIn;
+	}
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+	public void setCheckOut(LocalDate checkOut) {
+		this.checkOut = checkOut;
+	}
+	public Integer getNoOfPeople() {
+		return noOfPeople;
+	}
+	public void setNoOfPeople(Integer noOfPeople) {
+		this.noOfPeople = noOfPeople;
+	}
+	public Float getTotalCost() {
+		return totalCost;
+	}
+	public void setTotalCost(Float totalCost) {
+		this.totalCost = totalCost;
+	}
+	public LocalDateTime getTimeOfBooking() {
+		return timeOfBooking;
+	}
+	public void setTimeOfBooking(LocalDateTime timeOfBooking) {
+		this.timeOfBooking = timeOfBooking;
+	}
+	public Destination getDestinationEntity() {
+		return destinationEntity;
+	}
+	public void setDestinationEntity(Destination destinationEntity) {
+		this.destinationEntity = destinationEntity;
+	}
+	public User getUserEntity() {
+		return userEntity;
+	}
+	public void setUserEntity(User userEntity) {
+		this.userEntity = userEntity;
+	}
+	
+}
