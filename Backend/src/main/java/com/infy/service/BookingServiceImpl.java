01d38c54a2f9f2d4502291fe96c6e4f7b998ae9b
Sristy <kalasristy@gmail.com>
2022-09-24 20:17:52 +0530

    Modified Service implementation class

--- Backend/src/main/java/com/infy/service/BookingServiceImpl.java
+++ Backend/src/main/java/com/infy/service/BookingServiceImpl.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.io.Exception;
+import java.io.*;
 
 import javax.transaction.Transactional;
 
@@ -24,7 +24,6 @@ import com.infy.repository.BookingRepository;
 import com.infy.repository.DestinationRepository;
 import com.infy.repository.UserRepository;
 
-
 @Service(value = "bookingService")
 @Transactional
 public class BookingServiceImpl implements BookingService{
@@ -39,23 +38,128 @@ public class BookingServiceImpl implements BookingService{
 	private DestinationRepository destinationRepository;
 	
 	
-	@Override
+//	@Override
 	public Integer addBooking(BookingDTO bookingDTO,Integer userId,String destinationId) throws Exception {
 		
+		try {
+		Booking booking = new Booking();
+		booking.setCheckIn(bookingDTO.getCheckIn());
+		booking.setNoOfPeople(bookingDTO.getNoOfPeople());
+		booking.setTotalCost(bookingDTO.getTotalCost());
+		LocalDateTime date=LocalDateTime.now();
+		booking.setTimeOfBooking(date);
+		
+		Destination destination=destinationRepository.findByDestinationId(destinationId);
+		booking.setCheckOut(bookingDTO.getCheckIn().plusDays(destination.getNoOfNights()));
+		Optional<User> optional=userRepository.findById(userId);
+		User user=optional.orElseThrow(() -> new WanderLustException("BookingService.INVALID_BOOKINGS"));
+		booking.setUserEntity(user);
+		if(destination.getAvailability() >= booking.getNoOfPeople()) {
+			destination.setAvailability(destination.getAvailability() - booking.getNoOfPeople());
+			booking.setDestinationEntity(destination);
+		}
+		
+		bookingRepository.save(booking);
+		return booking.getBookingId();
+		}
 	}
 	
 	public List<BookingDTO> getViewBooking(Integer userId) throws Exception {
 		
+		List<Booking> bookingList = bookingRepository.findByBookUserId(userId);
+		List<BookingDTO> bookingDtoList = new ArrayList<BookingDTO>();
 		
+		for(Booking booking: bookingList) {
+			BookingDTO bookingDTO = new BookingDTO();
+			bookingDTO.setBookingId(booking.getBookingId());
+			bookingDTO.setCheckIn(booking.getCheckIn());
+			bookingDTO.setCheckOut(booking.getCheckOut());
+			bookingDTO.setNoOfPeople(booking.getNoOfPeople());
+			bookingDTO.setTimeOfBooking(booking.getTimeOfBooking());
+			bookingDTO.setTotalCost(booking.getTotalCost());
+			
+			DestinationDTO destinationDTO = new DestinationDTO();
+			destinationDTO.setAvailability(booking.getDestinationEntity().getAvailability());
+			destinationDTO.setChargePerPerson(booking.getDestinationEntity().getChargePerPerson());
+			destinationDTO.setContinent(booking.getDestinationEntity().getContinent());
+			destinationDTO.setDestinationId(booking.getDestinationEntity().getDestinationId());
+			destinationDTO.setDestinationName(booking.getDestinationEntity().getDestinationName());
+			destinationDTO.setDiscount(booking.getDestinationEntity().getDiscount());
+			destinationDTO.setFlightCharge(booking.getDestinationEntity().getFlightCharge());
+			destinationDTO.setImageUrl(booking.getDestinationEntity().getImageUrl());
+			destinationDTO.setNoOfNights(booking.getDestinationEntity().getNoOfNights());
+			
+			DetailsDTO detailsDTO = new DetailsDTO();
+			detailsDTO.setAbout(booking.getDestinationEntity().getDetailsEntity().getAbout());
+			detailsDTO.setDetailsId(booking.getDestinationEntity().getDetailsEntity().getDetailsId());
+			detailsDTO.setPace(booking.getDestinationEntity().getDetailsEntity().getPace());
+			detailsDTO.setPackageInclusion(booking.getDestinationEntity().getDetailsEntity().getPackageInclusion());
+			
+			ItineraryDTO itineraryDTO = new ItineraryDTO();
+			itineraryDTO.setFirstDay(booking.getDestinationEntity().getDetailsEntity().getItineraryEntity().getFirstDay());
+			itineraryDTO.setItineraryId(booking.getDestinationEntity().getDetailsEntity().getItineraryEntity().getItineraryId());
+			itineraryDTO.setLastDay(booking.getDestinationEntity().getDetailsEntity().getItineraryEntity().getLastDay());
+			itineraryDTO.setRestOfDays(booking.getDestinationEntity().getDetailsEntity().getItineraryEntity().getRestOfDays());
+			
+			detailsDTO.setItinerary(itineraryDTO);
+			destinationDTO.setDetailsDTO(detailsDTO);
+			bookingDtoList.add(bookingDTO);
+		}
+		
+		return bookingDtoList;
 	}
 	
-	public Float cancelBooking(Integer bookingId) throws WanderLustException {
+	public Float cancelBooking(Integer bookingId) throws Exception {
 		
+		Float refundAmount=null;
+
+		Optional<Booking> optional=bookingRepository.findById(bookingId);
+
+		booking.getDestinationEntity().setAvailability(booking.getDestinationEntity().getAvailability()+booking.getNoOfPeople());
+		booking.setDestinationEntity(null);
+		booking.setUserEntity(null);
 		
+		refundAmount=booking.getTotalCost();
+		bookingRepository.delete(booking);
+		return refundAmount;
 	}
 	
 	public DestinationDTO getDestinationDetails(String destinationId) throws Exception {
 		
+		Destination destination=destinationRepository.findByDestinationId(destinationId);
+		DestinationDTO destinationDTO=new DestinationDTO();
+			
+		if(destination!=null)
+		{
+			destinationDTO.setAvailability(destination.getAvailability());
+			destinationDTO.setChargePerPerson(destination.getChargePerPerson());
+			destinationDTO.setContinent(destination.getContinent());
+			destinationDTO.setDestinationId(destination.getDestinationId());
+			destinationDTO.setDestinationName(destination.getDestinationName());
+			destinationDTO.setDiscount(destination.getDiscount());
+			destinationDTO.setFlightCharge(destination.getFlightCharge());
+			destinationDTO.setImageUrl(destination.getImageUrl());
+			destinationDTO.setNoOfNights(destination.getNoOfNights());
+			
+			Details detailsEntity = destination.getDetailsEntity();
+			DetailsDTO detailsDTO = new DetailsDTO();
+			detailsDTO.setAbout(detailsEntity.getAbout());
+			detailsDTO.setDetailsId(detailsEntity.getDetailsId());
+			detailsDTO.setHighlights(detailsEntity.getHighlights());
+			detailsDTO.setPace(detailsEntity.getPace());
+			detailsDTO.setPackageInclusion(detailsEntity.getPackageInclusion());
+			
+			Itinerary itineraryEntity = destination.getDetailsEntity().getItineraryEntity();
+			ItineraryDTO itineraryDTO = new ItineraryDTO();
+			itineraryDTO.setFirstDay(itineraryEntity.getFirstDay());
+			itineraryDTO.setLastDay(itineraryEntity.getLastDay());
+			itineraryDTO.setItineraryId(itineraryEntity.getItineraryId());
+			itineraryDTO.setRestOfDays(itineraryEntity.getRestOfDays());
+			
+			detailsDTO.setItinerary(itineraryDTO);
+			destinationDTO.setDetailsDTO(detailsDTO);
+			return destinationDTO;
+		}
 	}
 	
 }
