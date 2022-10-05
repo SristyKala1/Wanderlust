0ad55e5b057d8f71b4e2a4c3359aca624591bb0f
Sristy <kalasristy@gmail.com>
2022-10-06 02:05:07 +0530

    modified service worker file

--- Frontend/src/serviceWorker.js
+++ Frontend/src/serviceWorker.js
@@ -127,3 +127,11 @@ function checkValidServiceWorker(swUrl, config) {
       );
     });
 }
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+    });
+  }
+}
