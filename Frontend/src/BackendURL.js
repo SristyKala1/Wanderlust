c5f40df56b268595e98c3826af2c462d312e2114
Sristy <kalasristy@gmail.com>
2022-10-05 12:38:32 +0530

    Added backend urls

---
+++ Frontend/src/BackendURL.js
@@ -0,0 +1,31 @@
+export const backendUrlLogin = "http://localhost:4000/Wanderlust_Server/UserAPI/userLogin";
+export const backendUrlPackages = "http://localhost:4000/Wanderlust_Server/DestinationAPI/packages/";
+export const backendUrlRegister = "http://localhost:4000/Wanderlust_Server/UserAPI/userRegister/";
+export const backendUrlBooking = "http://localhost:4000/Wanderlust_Server/BookingAPI/booking/";
+
+export const backendUrlHotdeals = "http://localhost:4000/Wanderlust_Server/DestinationAPI/packages";
+
+export const backendUrlDestination_continent = "http://localhost:4000/Wanderlust_Server/DestinationAPI/packages/{continent}";
+export const backendUrlDestination_packages = "http://localhost:4000/Wanderlust_Server/DestinationAPI/packages";
+
+export const backendUrlRegistration_userRegister = "http://localhost:4000/Wanderlust_Server/RegistrationAPI/userRegister";
+export const backendUrlRegistration_viewBooking = "http://localhost:4000/Wanderlust_Server/BookingAPI/viewBooking/{userId}";
+
+export const backendUrlUser_userLogin = "http://localhost:4000/Wanderlust_Server/UserAPI/userLogin/";
+
+export const backendUrlBooking_checkItinerary = "http://localhost:4000/Wanderlust_Server/BookingAPI/checkItinerary/{destinationId}";
+export const backendUrlBooking_delete = "http://localhost:4000/Wanderlust_Server/BookingAPI/delete/{bookingId}";
+
+
+
+
+export const backendUrlCheckItinerary = "http://localhost:4000/Wanderlust_Server/BookingAPI/checkItinerary/";
+
+export const backendUrlListOfBooking = "http://localhost:4000/Wanderlust_Server/BookingAPI/viewBooking/";
+export const backendUrlDeleteBooking = "http://localhost:4000/Wanderlust_Server/BookingAPI/deleteBooking/";
+
+
+export const backendUrlSearchReview = "http://localhost:4000/Wanderlust_Server/ReviewAPI/searchReview/";
+export const backendUrlViewReview = "http://localhost:4000/Wanderlust_Server/ReviewAPI/viewReview/";
+export const backendUrlAddReview = "http://localhost:4000/Wanderlust_Server/ReviewAPI/addReview/";
+
