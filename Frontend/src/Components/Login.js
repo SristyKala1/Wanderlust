41fc795d23c30d796fe853474186f7c750385e5a
Sristy <kalasristy@gmail.com>
2022-10-05 13:21:19 +0530

    Added code for login page and updated backend urls

---
+++ Frontend/src/Components/Login.js
@@ -0,0 +1,238 @@
+import React, { Component } from "react";
+import "../custom.css";
+import { Redirect } from "react-router-dom";
+import { Users } from "./models/User";
+import axios from "axios";
+import { Message } from "primereact/message";
+import { InputText } from "primereact/inputtext";
+import { backendUrlLogin } from "../BackendURL";
+
+class Login extends Component {
+  state = {
+    successMessage: "",
+    register: false,
+    errorMessage: null,
+    loginForm: {
+      contactNo: "",
+      password: ""
+    },
+    contactNo: "",
+    contactErrorMessage: "",
+    password: "",
+    passwordErrorMessage: "",
+    validPassWord: false,
+    validContactNumber: false
+  };
+
+  login = event => {
+    event.preventDefault();
+    var contactNumber = document.getElementById("contactNumber").value;
+    var password = document.getElementById("password").value;
+    var user = new Users();
+    user.contactNumber = contactNumber;
+    user.password = password;
+    axios
+      .post(backendUrlLogin, user)
+      .then(response => {
+        this.setState({
+          register: false,
+          successMessage: "user logged in"
+        });
+        sessionStorage.setItem("userId", response.data.userId);
+        sessionStorage.setItem("userName", response.data.userName);
+        window.location.reload();
+      })
+      .catch(err => {
+        this.setState({
+          successMessage: null,
+          errorMessage: err
+        });
+      });
+  };
+
+  cancel = () => {
+    var contactNumber = null;
+    var password = null;
+  };
+
+  register = () => {
+    this.setState({
+      register: true
+    });
+    console.log(this.state.register);
+  };
+
+  validate = event => {
+    var fieldName = event.target.name;
+    var value = event.target.value;
+    var errorMessage = "";
+    switch (fieldName) {
+      case "contactNo":
+        errorMessage = "field required";
+        if (value) {
+          var regex = new RegExp(/^[6-9][0-9]{9}$/);
+          console.log(regex.test(value));
+          regex.test(value)
+            ? (errorMessage = "")
+            : (errorMessage += " Should be a valid indian number");
+          if (errorMessage != "field required") {
+            this.setState({ contactErrorMessage: errorMessage });
+          } else {
+            this.setState({
+              contactErrorMessage: "",
+              validContactNumber: true
+            });
+          }
+         
+        }
+         this.setState({
+            contactNo: value
+          });
+        break;
+      case "password":
+        errorMessage = "field required";
+        if (value) {
+          var regexCAp = new RegExp(/^.*[A-Z].*$/);
+          var regexLow = new RegExp(/^.*[a-z].*$/);
+          var regexNum = new RegExp(/^.*[0-9].*$/);
+          regexCAp.test(value)
+            ? (errorMessage = "")
+            : (errorMessage =
+                errorMessage + " Should contain atleast 1 upper case letter");
+          regexLow.test(value)
+            ? (errorMessage = "")
+            : (errorMessage =
+                errorMessage + " Should contain atleast 1 lower case letter ");
+          regexNum.test(value)
+            ? (errorMessage = "")
+            : (errorMessage =
+                errorMessage + " Should contain atleast 1 number");
+          if (errorMessage != "field required") {
+            this.setState({ passwordErrorMessage: errorMessage });
+          } else {
+            this.setState({
+              passwordErrorMessage: "",
+              validPassWord: true
+            });
+          }
+
+         
+        } this.setState({
+            password: value
+          });
+        break;
+    }
+  };
+  render() {
+    if (this.state.register === true) return <Redirect to={"/register"} />;
+    else if (this.state.successMessage != "") return <Redirect to={"/"} />;
+    return (
+      <div className="terms-bg">
+        <div className="row my-0">
+          <section className="col"></section>
+          <section className="col">
+          {/* <h2>Welcome to WanderLust</h2> */}
+          <div className='card shadow my-5'>
+          <div className="card-header bg-info text-white text-center">
+          <h5>Welcome to WanderLust</h5>
+          </div>
+          <div className="card-body  px-5">
+            <form className="form form-horizontal">
+            
+              <div className="form-group my-0 ">
+              <label htmlFor="contactNumber" className='text-muted float-left'>
+                      Contact Number
+                    </label>
+                <span className="p-float-label">
+                  <InputText
+                    id="contactNumber" 
+                    required
+                    type="text" 
+                    min="6000000000" 
+                    max="9999999999"
+                    name="contactNo" 
+                    placeholder="Enter Contact Number"
+                    autoComplete="off"
+                    onChange={this.validate}
+                    className="form-control"
+                    value={this.state.contactNo}
+                  />
+                  
+                </span>
+                {this.state.contactErrorMessage ? (
+                  <Message
+                    severity="error"
+                    text={this.state.contactErrorMessage}
+                  />
+                ) : null}
+              </div>
+              <br />
+              <div className="form-group my-0">
+              <label htmlFor="password" className="text-muted float-left">
+                      Password
+                    </label>
+                <span className="p-float-label">
+                  <InputText
+                    id="password" 
+                    value={this.state.loginForm.password}
+                    required 
+                    type="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    className="form-control"
+                    onChange={this.validate}
+                    value={this.state.password}
+                  />
+                  
+                </span>
+                {this.state.passwordErrorMessage ? (
+                  <Message
+                    severity="error"
+                    text={this.state.passwordErrorMessage}
+                  />
+                ) : null}
+              </div>
+              <br />
+              <div className="form-group mt-3">
+                <div className="row">
+                  <div className="col">
+                    <button
+                      type="submit"
+                      className="btn btn-success form-control"
+                      onClick={this.login}
+                    >
+                      Login
+                    </button>
+                  </div>
+                  <div className="col">
+                    <button
+                      type="submit"
+                      className="col btn btn-danger form-control"
+                    >
+                      Back
+                    </button>
+                  </div>
+                </div>
+                <div>
+                <span><p className="text-dark text-info mt-3">Don't have an account?</p></span>
+                </div>
+                <button
+                  type="submit"
+                  className="col btn btn-primary form-control my-0"
+                  onClick={this.register}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+            </div>
+          </div>
+          </section>
+          <section className="col"></section>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
