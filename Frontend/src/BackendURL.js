d338e60cc74297033b0248029e0276ecfa4f1131
Sristy <kalasristy@gmail.com>
2022-10-05 14:02:57 +0530

    Modified backend urls

--- Frontend/src/BackendURL.js
+++ Frontend/src/BackendURL.js
@@ -1,9 +1,12 @@
 export const backendUrlLogin = "http://localhost:4000/Wanderlust_Server/UserAPI/userLogin";
 export const backendUrlBooking = "http://localhost:4000/Wanderlust_Server/BookingAPI/booking/";
+export const backendUrlPackages = "http://localhost:4000/Wanderlust_Server/DestinationAPI/packages/";
 
 export const backendUrlBooking_checkItinerary = "http://localhost:4000/Wanderlust_Server/BookingAPI/checkItinerary/{destinationId}";
 
 export const backendUrlCheckItinerary = "http://localhost:4000/Wanderlust_Server/BookingAPI/checkItinerary/";
+
+export const backendUrlViewReview = "http://localhost:4000/Wanderlust_Server/ReviewAPI/viewReview/";
 
 
 
